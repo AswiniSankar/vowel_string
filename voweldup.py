44875de30698fcf536c2bb577f3f19646f1ff461
Aswini Sankar <53120389+AswiniSankar@users.noreply.github.com>
2020-08-18 10:17:29 +0530

    Add files via upload

---
+++ voweldup.py
@@ -0,0 +1,12 @@
+'''
+ip:tamil
+op:taamiil
+'''
+s=input()
+for i in s:
+ if i=='a' or i=='e' or i=='i' or i=='o' or i=='u':
+   print(i,end='')
+   print(i,end='')
+ else:
+   print(i,end='')
+print("\r")
